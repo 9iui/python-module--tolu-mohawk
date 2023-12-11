1e8bdbfc3b31a02c65bce420d539463c31ef1f69
9iui <baderotolu@ymail.com>
2023-12-10 21:25:24 -0500

    changes to code base

--- decrypt.py
+++ decrypt.py
@@ -3,25 +3,31 @@ import argparse
 import base64
 
 def decrypt(input_file, key):
+    # Create a Fernet cipher suite with the provided key
     cipher_suite = Fernet(base64.urlsafe_b64encode(key))
 
+    # Read the encrypted message from the specified input file
     with open(input_file, 'rb') as file:
         encrypted_message = file.read()
 
+    # Decrypt the message
     decrypted_message = cipher_suite.decrypt(encrypted_message)
+
+    # Print the decrypted message to the console
     print("Decrypted Message:", decrypted_message.decode())
 
 def main():
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description="Decrypt a message from a file using a key.")
     parser.add_argument("--input", required=True, help="The input file path")
     parser.add_argument("--key", required=True, help="The decryption key")
-
     args = parser.parse_args()
 
     # Ensure the key is 32 bytes long
     key = base64.urlsafe_b64encode(args.key.encode())
     key = key.ljust(32, b'=')
 
+    # Call the decryption function
     decrypt(args.input, key)
 
 if __name__ == "__main__":
--- encrypt.py
+++ encrypt.py
@@ -3,24 +3,29 @@ import argparse
 import base64
 
 def encrypt(message, key, output_file):
+    # Create a Fernet cipher suite with the provided key
     cipher_suite = Fernet(base64.urlsafe_b64encode(key))
+
+    # Encrypt the message
     encrypted_message = cipher_suite.encrypt(message.encode())
 
+    # Write the encrypted message to the specified output file
     with open(output_file, 'wb') as file:
         file.write(encrypted_message)
 
 def main():
+    # Parse command line arguments
     parser = argparse.ArgumentParser(description="Encrypt a message and write it to a file.")
     parser.add_argument("--message", required=True, help="The message to encrypt")
     parser.add_argument("--key", required=True, help="The encryption key")
     parser.add_argument("--output", required=True, help="The output file path")
-
     args = parser.parse_args()
 
     # Ensure the key is 32 bytes long
     key = base64.urlsafe_b64encode(args.key.encode())
     key = key.ljust(32, b'=')
 
+    # Call the encryption function
     encrypt(args.message, key, args.output)
 
 if __name__ == "__main__":
